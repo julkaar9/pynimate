5f1ff76b27f95afd933cb935c7c2459a9637fca9
Md Julkarnaeen <32738417+julkaar9@users.noreply.github.com>
2023-04-09 02:40:32 +0530

    :arrow_up: added base, barh and new datafiers

--- src/pynimate/__init__.pyi
+++ src/pynimate/__init__.pyi
@@ -1,5 +1,15 @@
-from .bar import Barplot
-from .canvas import Canvas
-from .datafier import Datafier
+from pynimate.bar import Barplot
+from pynimate.barhplot import Barhplot
+from pynimate.baseplot import Baseplot
+from pynimate.canvas import Canvas
+from pynimate.datafier import BarDatafier, BaseDatafier, Datafier
 
-__all__ = ["Canvas", "Barplot", "Datafier"]
+__all__ = [
+    "Canvas",
+    "Barplot",
+    "Datafier",
+    "Baseplot",
+    "Barhplot",
+    "BaseDatafier",
+    "BarDatafier",
+]
