50b480a6569e13acf20e83f7f00fc74c62bfefb2
Md Julkarnaeen <32738417+julkaar9@users.noreply.github.com>
2023-04-13 01:39:52 +0530

    removed unused col_zorder

--- examples/example1.py
+++ examples/example1.py
@@ -15,22 +15,8 @@ df = pd.DataFrame(
 ).set_index("time")
 
 cnv = nim.Canvas()
-bar = nim.Barhplot.from_df(df, "%Y-%m-%d", "3d", rounded_edges=True)
-bar.set_time(callback=lambda i, datafier: datafier.data.index[i].year)
+bar = nim.Barhplot.from_df(df, "%Y-%m-%d", "2d")
+bar.set_time(callback=lambda i, datafier: datafier.data.index[i].strftime("%b, %Y"))
 cnv.add_plot(bar)
 cnv.animate()
 plt.show()
-# cols = ["r", "g", "b"]
-# vals = [1, 2, 2.5]
-# ranks = [1, 2, 2.5]
-# col_zorder = {"r": 1, "g": 3, "b": 2}
-# fig, ax = plt.subplots()
-# ax.barh(ranks, vals, tick_label=cols, color=cols)
-# for z, patch in zip(col_zorder.values(), ax.patches):
-#     patch.set_zorder(z)
-#     print(patch, patch.zorder)
-# print()
-# for z, patch in zip(col_zorder.values(), ax.patches):
-#     print(patch, patch.zorder)
-
-# plt.show()
